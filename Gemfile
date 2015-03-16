031d5496e65b47d4e841b27fb59fc737406daca1
DVG <devryguy@gmail.com>
2015-03-16 19:50:45 -0400

    Initial EDI Implementation

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'edi'
+group :development, :test do
+  gem 'byebug'
+end
---
+++ bot/server.rb
@@ -0,0 +1,6 @@
+class Server < EDI::Server
+  # Use `register_services` to enable services in your EDI Bot. Available built-in services are:
+  # :dice, :eightball, :fact, :giphy, :i_heart_quotes
+  # You can make your own services with `edi generate service my_service`
+  register_services :fact, :img_flip, :i_heart_quotes, :dice, :eightball, :giphy, :weather, :tweet_that
+end
