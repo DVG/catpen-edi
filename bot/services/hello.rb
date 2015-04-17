340bd3245a84502a876b46f20a83c54a5324b701
DVG <devryguy@gmail.com>
2015-04-16 22:15:50 -0400

    Hello Service

--- bot/services/hello.rb
+++ bot/services/hello.rb
@@ -1,6 +1,10 @@
 class Hello < EDI::Service
+  phrases "hi", "hello", "good morning", "good afternoon", "good evening"
   # See https://github.com/DVG/edi for information on creating services
   def run
-    "Hi There!"
+    [
+      "Hi There!",
+      "Hello"
+    ].sample
   end
 end
